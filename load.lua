5c44b277a099ec4085038844d09ebe45154a439b
ElliotDoesCode <59733209+ElliotDoesCode@users.noreply.github.com>
2021-03-07 14:24:52 -0500

    Update load.lua

--- load.lua
+++ load.lua
@@ -112,7 +112,7 @@ end)
 for _,v in pairs(game:GetService("Players"):GetPlayers()) do
 	if v.Team and v.Team.Name == "Criminals" then
 		local target = v.Character.UpperTorso
-		go(target +Vector3.new(0,3,0))
+		go(target.CFrame + Vector3.new(0,3,0))
 		if game.Players.LocalPlayer.Character.Humanoid.Sit == true then
 			leave()
 			wait(.3)
