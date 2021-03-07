567af520bc6746d5f7729d4475af099e60bd15f1
ElliotDoesCode <59733209+ElliotDoesCode@users.noreply.github.com>
2021-03-07 16:43:03 -0500

    Update load.lua

--- load.lua
+++ load.lua
@@ -137,7 +137,7 @@ while wait() do
 	pcall(function()
 		local x = {}
 		for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-			if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId and v.playing > 18 then
+			if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId and v.playing > 16 then
 				x[#x + 1] = v.id
 			end
 		end
